e2dddc897d90cf0d53001ef8531798ef43d6c5f6
yotke <38344544+yotke@users.noreply.github.com>
2021-03-09 12:49:52 +0200

    Delete saparetor_wav_file.m

--- help_matlab_scripts/saparetor_wav_file.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = saparetor_wav_file(concatenate_wav_file,names_array,length_array,)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
