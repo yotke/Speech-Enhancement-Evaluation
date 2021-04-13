9723d0317c68f4f644f5eab74a73d685875629d4
yotke <38344544+yotke@users.noreply.github.com>
2021-04-13 10:46:43 +0300

    Add files via upload

--- help_matlab_scripts/add_noise.m
+++ help_matlab_scripts/add_noise.m
@@ -1,5 +1,5 @@
 %add noise to audio files
-myDir = '/home/yotam/softwares/project/LibriTTS/part_wav_files';
+myDir = strcat(pwd,'/part_wav_files');
 ratio =5; % ratio of signal to noise.
 db_ratio=10^(ratio/20);
 [babble_noise,Fs_b]= audioread('/home/yotam/softwares/project/matlab/bubble noises/babble.wav');
@@ -7,13 +7,13 @@ db_ratio=10^(ratio/20);
 myFiles = dir(fullfile(myDir,'*.wav')); %gets all flac files in struct
 for k = 1:length(myFiles)
     
-    name = fullfile('/home/yotam/softwares/project/LibriTTS/part_wav_files',myFiles(k).name);
+    name = fullfile(strcat(pwd,'/part_wav_files'),myFiles(k).name);
     
     [y_clean,Fs] = audioread(name);%Read all audio set from clean directory
     babble_noised_signal = y_clean+ babble_noise(1+1000*k:1000*k+length(y_clean))/db_ratio; %+ babble_noise(10*k:10*k+length(y_clean))/db_ratio
      
     % Write to a new file outside matlab.
-    fout_name = fullfile('/home/yotam/softwares/project/matlab/noised/babble_noise',myFiles(k).name);
+    fout_name = fullfile(strcat(pwd,'/noised/babble_noise'),myFiles(k).name);
     audiowrite(fout_name,babble_noised_signal,Fs)
     
     sigma = rms(y_clean)/db_ratio;
@@ -22,17 +22,8 @@ for k = 1:length(myFiles)
     
     
     %Write to a new file outside matlab.
-    fout_name = fullfile('/home/yotam/softwares/project/matlab/noised/white_noise',myFiles(k).name);
+    fout_name = fullfile(strcat(pwd,'/noised/white_noise'),myFiles(k).name);
     audiowrite(fout_name,white_noised_signal,Fs)
     
 end
 
-
-
-
-%sound(z,Fs)
-%S_im=imnoise(y_clean,'Gaussian',Mean,Var);
-%out=awgn(y_clean,27); % using matlab to create a normal distribution
-%fout = strcat(fout_name,'.wav');
-%y_i = strcat('y_',out_num); % a new dateset
-%myDir = uigetdir; %gets directory
